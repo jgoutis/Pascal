c6d6f9944ae80246cfc9c3b0cc26ef7f9a40efaf
jgoutis <111982869+jgoutis@users.noreply.github.com>
2024-09-30 14:27:58 +0200

    Update and rename HelloWorld.java to Pascal.java

--- Pascal.java
+++ Pascal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class HelloWorld {
+public class Pascal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Degree of binomial expansion? ");
